02c5d055e4f445c86276374800e9606b1b170796
Sasa Hrubec <sasa.hrubec@akd.hr>
2014-11-21 11:44:06 +0100

    Slogan dorada

--- LotterySimulatorTomcat/src/main/java/hr/shrubec/simulator/facade/SloganFacade.java
+++ LotterySimulatorTomcat/src/main/java/hr/shrubec/simulator/facade/SloganFacade.java
@@ -13,29 +13,28 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class SloganFacade {
 
-	private final List<String> lista;
+	@SuppressWarnings("serial")
+	private static final List<String> lista = new ArrayList<String>() {
+	    {
+	    	add("\"All it takes is a dollar and another dollar.  Then another dollar.  And another...\"");
+			add("\"The Lottery... get rich or die trying!\"");
+			add("\"Hey, crazier things have happened!\"");
+			add("\"Work?  Save?  Invest?  Screw That!\"");
+			add("\"The Lottery... if you don’t keep playing we’ll just raise your taxes.\"");
+			add("\"You play to win the Lottery!  You don’t play just to play it!\"");
+			add("\"Sure, you’re more likely to get hit by lightning... but the lottery is more fun.\"");
+			add("\"It could be you! (But don’t get your hopes up).\"");
+			add("\"There really is a sucker born every minute.\"");
+			add("\"You know you’d just throw your money away anyway.\"");
+	    }
+	};
 	
-	public SloganFacade() {
-		lista=new ArrayList<String>();
-		lista.add("\"All it takes is a dollar and another dollar.  Then another dollar.  And another...\"");
-		lista.add("\"The Lottery... get rich or die trying!\"");
-		lista.add("\"Hey, crazier things have happened!\"");
-		lista.add("\"Work?  Save?  Invest?  Screw That!\"");
-		lista.add("\"The Lottery... if you don’t keep playing we’ll just raise your taxes.\"");
-		lista.add("\"You play to win the Lottery!  You don’t play just to play it!\"");
-		lista.add("\"Sure, you’re more likely to get hit by lightning... but the lottery is more fun.\"");
-		lista.add("\"It could be you! (But don’t get your hopes up).\"");
-		lista.add("\"There really is a sucker born every minute.\"");
-		lista.add("\"You know you’d just throw your money away anyway.\"");
-	}
 
 	public String getSlogan() {
-		
 		Ranecu ranescu=new Ranecu(new Date());
 		Integer index=ranescu.choose(0, lista.size()-1);
 		return lista.get(index);
 	}
 
-	
 	
 }
